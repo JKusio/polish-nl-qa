d3f9c9078ac573d979d22418aa664bc262738280
JKusio <jakubkusiowski@gmail.com>
2024-06-24 22:57:05 +0200

    Add redis cache

---
+++ src/cache/cache.py
@@ -0,0 +1,14 @@
+import redis
+
+SIX_MONTHS = 60 * 60 * 24 * 30 * 6
+
+
+class Cache:
+    def __init__(self, cache_dir):
+        self.redis = redis.Redis(host="localhost", port=6379, db=0)
+
+    def get(self, key):
+        return self.redis.get(key)
+
+    def set(self, key, value):
+        return self.redis.set(key, value, SIX_MONTHS)
