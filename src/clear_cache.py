48d999c3eb94253678ed7353483b2699ba10e868
JKusio <jakubkusiowski@gmail.com>
2024-08-11 21:40:44 +0200

    Change clear cache to use mongodb

--- src/clear_cache.py
+++ src/clear_cache.py
@@ -1,20 +1,15 @@
-import redis
+from pymongo import MongoClient
 
-r = redis.StrictRedis(host="localhost", port=6379, db=0)
 
-# comment out the keys you want to keep
-prefixes = ["count:*", "vectorizer:*", "prompt:*", "reranker:*", "query:*", "score:*"]
+client = MongoClient("mongodb://localhost:27017/")
+db = client["polish-nl-qa"]
+collection_name = "key_value"
+# prefixes = ["count", "vectorizer", "prompt", "reranker", "query", "score"]
+prefixes = []
+collection = db[collection_name]
 
 for prefix in prefixes:
     print(f"Clearing keys with prefix: {prefix}")
-    cursor = 0
-    while True:
-        cursor, keys = r.scan(cursor, match=prefix)
-        print(f"Deleting {len(keys)} keys")
-        if len(keys) > 0:
-            r.delete(*keys)
-        elif cursor == 0:
-            break
-        else:
-            print("No keys found")
-            break
+    collection.delete_many({"key": {"$regex": f"^{prefix}:.*"}})
+
+client.close()
